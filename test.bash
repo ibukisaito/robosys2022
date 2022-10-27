bcb9d09fc34ffdb5f416f5419e7c8b092a778ba3
saito <ibuki1519@outlook.jp>
2022-10-27 12:58:04 +0900

    test script

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Ibuki Saito <s21c1050gb@s.chibakoudai.jp>
 # SPDX-License-Identifier: BSD-3-Clause
 ng(){
@@ -12,9 +12,16 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+### STRANGE INPUT ###
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+		    　 
 [ "$res" = 0 ] && echo OK
-
 exit $res
-
-
 
