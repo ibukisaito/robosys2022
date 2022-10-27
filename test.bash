2c8f63351031bbcf93a11de8b025666299be9292
saito <ibuki1519@outlook.jp>
2022-10-27 12:32:29 +0900

    test script

--- test.bash
+++ test.bash
@@ -1,7 +1,20 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Ibuki Saito <s21c1050gb@s.chibakoudai.jp>
+# SPDX-License-Identifier: BSD-3-Clause
+ng(){
+	echo NG at line $1
+	res=1
+}
 
+res=0
+
+###I/O TEST###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
 
 
 
