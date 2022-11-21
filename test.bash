fe2f99bcbb2f39e41f880d7fd324d9a98cfd9e57
Ibuki Saito <myile1341@outlook.jp>
2022-11-22 01:06:55 +0900

    add a command

--- test.bash
+++ test.bash
@@ -9,11 +9,8 @@ ng(){
 res=0
 
 ###I/O TEST###
-out=$(seq 5 | ./average)
-[ "${out}" = 3.0 ] || ng ${LINENO}
-
-out=$(seq 0 | ./average)
-[ "${out}" = Error ] || ng ${LINENO}
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 
